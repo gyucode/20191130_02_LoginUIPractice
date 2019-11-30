20202d680aac49b80d3c95bfa030ea4d1919f1e6
gyucode <gyucode@gmail.com>
2019-11-30 16:21:35 +0900

    로그인화면 따라그리기 마무리2

--- app/src/main/java/kr/co/tjoeun/a20191130_02_loginuipractice/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191130_02_loginuipractice/MainActivity.kt
@@ -2,11 +2,21 @@ package kr.co.tjoeun.a20191130_02_loginuipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 자바와는 다르게 set/get 간소화
+        loginBtn.text = "Log in"
+
+        //나중에 변경 가능
+        var num1=10
+
+        //변경불가
+        val pi =3.14
     }
 }
